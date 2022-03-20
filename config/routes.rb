dc3086436805af783f28db385fa52ac5894a9bd5
ayrton <ayrton.parkinson1@gmail.com>
2022-03-20 08:38:46 -0400

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/articles', to: 'articles#index'
+  root "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/articles', to: 'articles#index'
 end
